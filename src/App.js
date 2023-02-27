0a77ea7b23eb4271f41a353caa42ddc581284943
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-02-27 22:46:09 +0530

    Removed Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/home/Home';
-import About from './pages/about/About';
 import Footer from './components/Footer'
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
-        <Route path="/about" element={ <About/> } />
       </Routes>
       <Footer />
     </div>
