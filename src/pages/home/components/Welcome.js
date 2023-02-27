7d800431cb9d687d6d14020d6222bf5aadc171f3
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-02-27 22:30:42 +0530

    Welcome Section

--- src/pages/home/components/Welcome.js
+++ src/pages/home/components/Welcome.js
@@ -3,7 +3,18 @@ import './Welcome.css'
 
 function Welcome() {
   return (
-    <div className='Welcome'>Welcome</div>
+    <div className='Welcome'>
+      <h1 className='heading'>
+        Unleash your <span>creativity</span> and <span>code</span><br />
+        your way to succeed in <br />
+        our hackathon!
+      </h1>
+      <p className='content'>
+        Welcome to Hackathena, the flagship hackathon organized by the Computer Science
+        and Engineering department and coordinated by the Computer Engineering Students
+        Association (CESA) of Jyothi Engineering College, Cheruthuruthy.
+      </p>
+    </div>
   )
 }
 
