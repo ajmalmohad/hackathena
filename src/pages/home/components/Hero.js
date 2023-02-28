a6cc0017e1b852446b9e61c6b99f99c6d79a4e16
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-02-28 20:03:00 +0530

    Hero Section

--- src/pages/home/components/Hero.js
+++ src/pages/home/components/Hero.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Hero.css'
+import Logo from './../../../assets/logo.png'
 
 function Hero() {
   return (
-    <div className='Hero'>Hero</div>
+    <div className='Hero'>
+      <main>
+        <div className='thelogo'>
+          <img src={Logo} alt="Logo" />
+          <div className='content'>
+            <h1>Hack<span>A</span>thena</h1>
+            <p>Hack the difference</p>
+          </div>
+        </div>
+        <div className='buttons'>
+          <button> Apply with Devfolio</button>
+          <button> Brochure</button>
+          <button> Join Discord</button>
+        </div>
+      </main>
+    </div>
   )
 }
 
