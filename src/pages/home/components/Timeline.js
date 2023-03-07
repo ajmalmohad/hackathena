21b4c46523efd5159c7dfefc2f983bfb89b58d28
SanjonRion <126318860+SanjonRion@users.noreply.github.com>
2023-03-07 22:35:21 +0530

    updates

--- src/pages/home/components/Timeline.js
+++ src/pages/home/components/Timeline.js
@@ -17,7 +17,7 @@ function Timeline() {
             title: "18/03/2023",
             cardTitle: `Round 1 Submission`,
             url:'https://docs.google.com/presentation/d/17adQbnaHA5qKjAgMvXtVxYT4QfY4UFKS/edit?usp=sharing&ouid=110435758198613008152&rtpof=true&sd=true',
-            cardDetailedText: `Round 1 is the first stage of competition where participants present their ideas in a ppt format to give an overview of the solution (Click here to download ppt).`
+            cardDetailedText: `Round 1 is the first stage of competition where participants present their ideas in a pdf format to give an overview of the solution(Whetherits in text or ppt submit it as pdf).`
           },
           { 
             title: "20/03/2023",
